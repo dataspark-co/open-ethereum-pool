7c0964cdb54557d67588361f422a018d584f8449
Nikolya <k_m_i@ukr.net>
2018-05-17 16:27:50 +0300

    test8

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -21,7 +21,7 @@ func (s *ProxyServer) handleLoginRPC(cs *Session, params []string, id string) (b
 	}
 
 	login := strings.ToLower(params[0])
-	log.Println(login)
+	log.Printf("Stratum miner login %v", login)
 
 	if !util.IsValidHexAddress(login) {
 		return false, &ErrorReply{Code: -1, Message: "Invalid login"}
